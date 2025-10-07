c89dde3927f63143b9606e435b3bd90905aa0408
Bogdan Sikora <bogdan@arokis.me>
2025-10-07 17:35:23 +0700

    Initial Commit

---
+++ VoiceOverDemo/VoiceOverDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceOverDemoApp.swift
+//  VoiceOverDemo
+//
+//  Created by Bogdan Sikora on 07.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct VoiceOverDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceOverDemoTests/VoiceOverDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceOverDemoTests.swift
+//  VoiceOverDemoTests
+//
+//  Created by Bogdan Sikora on 07.10.2025.
+//
+
+import Testing
+@testable import VoiceOverDemo
+
+struct VoiceOverDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
